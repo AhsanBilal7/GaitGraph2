8f1902044f9d08d30b2fbf8ecc78f4810f3e4f75
unacanal <woonhahaha@gmail.com>
2023-07-18 11:12:40 +0900

    [feat] Add new architecture (SpatioTemporalTransformer)

--- GaitGraph/models/transformer/net.py
+++ GaitGraph/models/transformer/net.py
@@ -1,3 +1,5 @@
+import math
+import torch
 import torch.nn as nn
 
 class TransformerClassifier(nn.Module):
@@ -32,3 +34,82 @@ class TransformerClassifier(nn.Module):
 
         return x, feat
     
+
+class SpatioTemporalTransformer(nn.Module):
+    def __init__(self, d_model, nhead, num_layers, num_joints=18, num_coordinates=2, num_classes=128):
+        super(SpatioTemporalTransformer, self).__init__()
+        self.spatial_transformer = SpatialTransformer(num_joints, num_coordinates, d_model, nhead, num_layers)
+        self.temporal_transformer = TemporalTransformer(d_model, nhead, num_layers, num_classes)
+
+    def forward(self, x):
+        x = self.spatial_transformer(x)
+        x = self.temporal_transformer(x)
+        return x
+
+class SpatialTransformer(nn.Module):
+    def __init__(self, num_joints, num_coordinates, d_model, num_heads, num_layers):
+        super(SpatialTransformer, self).__init__()
+
+        self.num_joints = num_joints
+        self.num_coordinates = num_coordinates
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.num_layers = num_layers
+
+        self.positional_encoding = self._get_positional_encoding()
+
+        self.transformer_layers = nn.ModuleList([
+            nn.TransformerEncoderLayer(d_model, num_heads) for _ in range(num_layers)
+        ])
+
+    def _get_positional_encoding(self):
+        positional_encoding = torch.zeros(self.num_joints, self.num_coordinates, self.d_model)
+        pos = torch.arange(0, self.num_joints).unsqueeze(1).unsqueeze(2).float()
+        div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * -(math.log(10000.0) / self.d_model))
+        positional_encoding[:, :, 0::2] = torch.sin(pos * div_term)
+        positional_encoding[:, :, 1::2] = torch.cos(pos * div_term)
+        return positional_encoding
+
+    def forward(self, x):
+        batch_size, sequence_length, _, _ = x.size()
+
+        # Reshape input tensor
+        x = x.view(batch_size * sequence_length, self.num_joints, self.num_coordinates)
+
+        # Add positional encoding
+        x = x + self.positional_encoding
+
+        # Transformer layers
+        for i in range(self.num_layers):
+            x = self.transformer_layers[i](x)
+
+        return x
+
+class TemporalTransformer(nn.Module):
+    def __init__(self, d_model, num_heads, num_layers, num_classes):
+        super(TemporalTransformer, self).__init__()
+
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.num_layers = num_layers
+
+        self.transformer_layers = nn.ModuleList([
+            nn.TransformerEncoderLayer(d_model, num_heads) for _ in range(num_layers)
+        ])
+
+        self.fc = nn.Linear(d_model, num_classes)
+
+    def forward(self, x):
+        batch_size, sequence_length, num_joints, num_coordinates = x.size()
+
+        # Reshape input tensor
+        x = x.view(batch_size * sequence_length, num_joints, num_coordinates)
+
+        # Transformer layers
+        for i in range(self.num_layers):
+            x = self.transformer_layers[i](x)
+
+        # Final linear layer
+        x = self.fc(x)
+
+        return x
